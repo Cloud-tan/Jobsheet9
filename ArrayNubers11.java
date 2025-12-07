aa49ee1f5218f4a99deee226043cb12dcdbd6af9
Cloudtan <maranathajeremy@gmail.com>
2025-12-07 18:09:18 +0700

    2

--- ArrayNubers11.java
+++ ArrayNubers11.java
@@ -4,18 +4,16 @@ public class ArrayNubers11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] num = new int[4];
+        double[] num = new double[4];
 
-        num[0] = 5;
-        num[1] = 12;
-        num[2] = 7;
-        num[3] = 20;
+        num[0] = 5.0;
+        num[1] = 12867;
+        num[2] = 7.5;
+        num[3] = 20000000;
 
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
-
+        for (int i = 0; i < 4; i++){
+            System.out.println(num[i]);
+        }
         scanner.close();
         }
 }
