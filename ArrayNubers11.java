aea60b89b7871375f4750e687777e51c424a3074
Cloudtan <maranathajeremy@gmail.com>
2025-12-07 17:55:54 +0700

    1

---
+++ ArrayNubers11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+//CODE BY JEREMY
+public class ArrayNubers11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] num = new int[4];
+
+        num[0] = 5;
+        num[1] = 12;
+        num[2] = 7;
+        num[3] = 20;
+
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+
+        scanner.close();
+        }
+}
